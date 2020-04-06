327a0b324d487fbc7fad9bd32104bb28cc4fe6ae
Danish Danial <danial@square.localdomain>
2020-04-06 23:08:06 +0800

    Corret DATABASE spelling in config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 CSRF_SESSION_KEY = 'WEvZt&hk$8QKZD5'
 
-if os.environ.get('DATABSE_URL') is None:
+if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/patient_record'
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
