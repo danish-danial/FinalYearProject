e58c9f3e324eac90efe7aee379386b42cb24ee5b
Danish Danial <danial@square.localdomain>
2020-04-06 22:17:41 +0800

    Initial Commit

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,11 @@
+from flask import Flask
 import os
 
-from flask import Flask
-
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "this_is_a_secret_word"
 
 import application.views
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 80)
+    app.run()
