bd1cd9a130f95e67dca9dd5c1ef013fa99c61a45
Danish Danial <danial@square.localdomain>
2020-04-06 23:03:41 +0800

    Fourth Commit

--- config.py
+++ config.py
@@ -2,26 +2,8 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'WEvZt&hk$8QKZD5'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-class TestingConfig(Config):
-    TESTING = True
+CSRF_ENABLED = True
+CSRF_SESSION_KEY = 'WEvZt&hk$8QKZD5'
 
 if os.environ.get('DATABSE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/patient_record'
